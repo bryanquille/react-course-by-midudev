564cecfedd8219f94b75d8424978fb8573a794f4
Bryan Quille <103023942+bryanquille@users.noreply.github.com>
2026-03-02 21:53:19 -0500

    adding and modifying table from tremor

--- class-9/src/App.tsx
+++ class-9/src/App.tsx
@@ -1,6 +1,11 @@
+import ListOfUsers from "./components/ListOfUsers"
+
 function App() {
   return (
-    <div>CRUD</div>
+    <>
+      <h1 className="my-6 font-semibold text-3xl text-center">Nuestro Proyecto con Redux</h1>
+      <ListOfUsers />
+    </>
   )
 }
 
---
+++ class-9/src/components/ListOfUsers.tsx
@@ -0,0 +1,95 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeaderCell,
+  TableRow,
+} from '@tremor/react';
+
+const users = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'johndoe@email.com',
+    github: 'johndoe',
+  },
+  {
+    id: 2,
+    name: 'Jane Smith',
+    email: 'janesmith@email.com',
+    github: 'janesmith',
+  },
+  {
+    id: 3,
+    name: 'Bob Johnson',
+    email: 'bobjohnson@email.com',
+    github: 'bobjohnson',
+  },
+];
+
+export default function ListOfUsers() {
+  return (
+    <>
+      <div className="sm:flex sm:items-center sm:justify-between sm:space-x-10">
+        <div>
+          <h3 className="font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
+            Workspaces
+          </h3>
+          <p className="mt-1 text-tremor-default leading-6 text-tremor-content dark:text-dark-tremor-content">
+            Overview of all registered workspaces within your organization.
+          </p>
+        </div>
+      </div>
+      <Table className="mt-8">
+        <TableHead>
+          <TableRow className="border-b border-tremor-border dark:border-dark-tremor-border">
+            <TableHeaderCell className="text-tremor-content-strong dark:text-dark-tremor-content-strong">
+              ID
+            </TableHeaderCell>
+            <TableHeaderCell className="text-tremor-content-strong dark:text-dark-tremor-content-strong">
+              Name
+            </TableHeaderCell>
+            <TableHeaderCell className="text-tremor-content-strong dark:text-dark-tremor-content-strong">
+              Email
+            </TableHeaderCell>
+            <TableHeaderCell className="text-tremor-content-strong dark:text-dark-tremor-content-strong">
+              Acciones
+            </TableHeaderCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.map((item) => (
+            <TableRow key={item.id}>
+              <TableCell className="font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">
+                {item.id}
+              </TableCell>
+              <TableCell className='flex items-center'>
+                <img
+                  src={`https://unavatar.io/github/${item.github}`}
+                  alt={`Avatar de ${item.name}`}
+                  className='w-8 h-8 mr-2 rounded-full'
+                />
+                {item.name}
+              </TableCell>
+              <TableCell>{item.email}</TableCell>
+              <TableCell>
+                <button type="button">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                  </svg>
+
+                </button>
+                <button type="button">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                  </svg>
+                </button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  );
+}
