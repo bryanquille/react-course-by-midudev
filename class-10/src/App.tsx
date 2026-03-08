ccd45db07b6b8b3fe8e4b4fcf5932d490d36c4f3
Bryan Quille <103023942+bryanquille@users.noreply.github.com>
2026-03-08 15:22:09 -0500

    fetching 100 users from an api and  typing the results with quicktype

--- class-10/src/App.tsx
+++ class-10/src/App.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react"
+import type { UserType } from "./types"
+
 function App() {
+  const [users, setUsers] = useState<UserType[]>([])
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const response = await fetch('https://randomuser.me/api/?results=100')
+        const data = await response.json()
+        setUsers(data.results)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getUsers()
+  }, [])
+
   return (
     <div>
       <h1>Prueba Técnica</h1>
+      {
+        JSON.stringify(users)
+      }
     </div>
   )
 }
